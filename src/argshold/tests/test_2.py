9cae5e385fb82d9646a16164e72f3b33adbe5459
Johannes <johannes-programming@mailfence.com>
2025-01-17 21:04:12 +0100

    Version 0.1.1+local.4

--- src/argshold/tests/test_2.py
+++ src/argshold/tests/test_2.py
@@ -1,77 +1,99 @@
 import unittest
 
-from frozendict import frozendict
-
 from argshold.core import ArgumentHolder, FrozenArgumentHolder
 
 
 class TestArgumentHolder(unittest.TestCase):
-    def setUp(self):
-        self.args = [1, 2, 3]
-        self.kwargs = {"a": 10, "b": 20}
-        self.holder = ArgumentHolder(*self.args, **self.kwargs)
-
     def test_initialization(self):
-        self.assertEqual(self.holder.args, self.args)
-        self.assertEqual(self.holder.kwargs, self.kwargs)
+        args = (1, 2, 3)
+        kwargs = {"a": 10, "b": 20}
+        holder = ArgumentHolder(*args, **kwargs)
 
-    def test_set_args(self):
-        new_args = [4, 5, 6]
-        self.holder.args = new_args
-        self.assertEqual(self.holder.args, new_args)
+        self.assertEqual(holder.args, list(args))
+        self.assertEqual(holder.kwargs, kwargs)
 
-    def test_set_kwargs(self):
-        new_kwargs = {"x": 30, "y": 40}
-        self.holder.kwargs = new_kwargs
-        self.assertEqual(self.holder.kwargs, new_kwargs)
+    def test_equality(self):
+        holder1 = ArgumentHolder(1, 2, a=3, b=4)
+        holder2 = ArgumentHolder(1, 2, a=3, b=4)
+        holder3 = ArgumentHolder(1, 2, a=3)
 
-    def test_delete_args(self):
-        del self.holder.args
-        self.assertEqual(self.holder.args, [])
+        self.assertEqual(holder1, holder2)
+        self.assertNotEqual(holder1, holder3)
 
-    def test_delete_kwargs(self):
-        del self.holder.kwargs
-        self.assertEqual(self.holder.kwargs, {})
+    def test_argument_modification(self):
+        holder = ArgumentHolder(1, 2, 3, a=10, b=20)
 
-    def test_copy(self):
-        copy_holder = self.holder.copy()
-        self.assertIsInstance(copy_holder, ArgumentHolder)
-        self.assertEqual(copy_holder.args, self.args)
-        self.assertEqual(copy_holder.kwargs, self.kwargs)
+        holder.args = [4, 5, 6]
+        holder.kwargs = {"x": 30, "y": 40}
+
+        self.assertEqual(holder.args, [4, 5, 6])
+        self.assertEqual(holder.kwargs, {"x": 30, "y": 40})
 
     def test_len(self):
-        self.assertEqual(len(self.holder), len(self.args) + len(self.kwargs))
+        holder = ArgumentHolder(1, 2, 3, a=10, b=20)
+        self.assertEqual(len(holder), 5)
+
+    def test_call(self):
+        holder = ArgumentHolder(1, 2, a=3, b=4)
+        result = holder.call(lambda x, y, a, b: x + y + a + b)
+        self.assertEqual(result, 10)
+
+    def test_copy(self):
+        holder = ArgumentHolder(1, 2, a=3, b=4)
+        copied_holder = holder.copy()
+
+        self.assertEqual(copied_holder.args, holder.args)
+        self.assertEqual(copied_holder.kwargs, holder.kwargs)
+        self.assertIsNot(copied_holder, holder)
+
+    def test_conversion(self):
+        holder = ArgumentHolder(1, 2, a=3, b=4)
+        frozen_holder = holder.toFrozenArgumentHolder()
+
+        self.assertIsInstance(frozen_holder, FrozenArgumentHolder)
+        self.assertEqual(frozen_holder.args, tuple(holder.args))
+        self.assertEqual(frozen_holder.kwargs, holder.kwargs)
 
 
 class TestFrozenArgumentHolder(unittest.TestCase):
-    def setUp(self):
-        self.args = (1, 2, 3)
-        self.kwargs = frozendict({"a": 10, "b": 20})
-        self.holder = FrozenArgumentHolder(*self.args, **self.kwargs)
-
     def test_initialization(self):
-        self.assertEqual(self.holder.args, self.args)
-        self.assertEqual(self.holder.kwargs, self.kwargs)
+        args = (1, 2, 3)
+        kwargs = {"a": 10, "b": 20}
+        holder = FrozenArgumentHolder(*args, **kwargs)
 
-    def test_immutable_args(self):
-        with self.assertRaises(AttributeError):
-            self.holder.args = [4, 5, 6]
+        self.assertEqual(holder.args, args)
+        self.assertEqual(holder.kwargs, kwargs)
 
-    def test_immutable_kwargs(self):
-        with self.assertRaises(AttributeError):
-            self.holder.kwargs = {"x": 30, "y": 40}
+    def test_equality(self):
+        holder1 = FrozenArgumentHolder(1, 2, a=3, b=4)
+        holder2 = FrozenArgumentHolder(1, 2, a=3, b=4)
+        holder3 = FrozenArgumentHolder(1, 2, a=3)
 
-    def test_hash(self):
-        self.assertEqual(hash(self.holder), hash((self.args, self.kwargs)))
-
-    def test_copy(self):
-        copy_holder = self.holder.copy()
-        self.assertIsInstance(copy_holder, FrozenArgumentHolder)
-        self.assertEqual(copy_holder.args, self.args)
-        self.assertEqual(copy_holder.kwargs, self.kwargs)
+        self.assertEqual(holder1, holder2)
+        self.assertNotEqual(holder1, holder3)
 
     def test_len(self):
-        self.assertEqual(len(self.holder), len(self.args) + len(self.kwargs))
+        holder = FrozenArgumentHolder(1, 2, 3, a=10, b=20)
+        self.assertEqual(len(holder), 5)
+
+    def test_call(self):
+        holder = FrozenArgumentHolder(1, 2, a=3, b=4)
+        result = holder.call(lambda x, y, a, b: x + y + a + b)
+        self.assertEqual(result, 10)
+
+    def test_hash(self):
+        holder = FrozenArgumentHolder(1, 2, a=3, b=4)
+        hash_value = hash(holder)
+
+        self.assertIsInstance(hash_value, int)
+
+    def test_conversion(self):
+        holder = FrozenArgumentHolder(1, 2, a=3, b=4)
+        normal_holder = holder.toArgumentHolder()
+
+        self.assertIsInstance(normal_holder, ArgumentHolder)
+        self.assertEqual(normal_holder.args, list(holder.args))
+        self.assertEqual(normal_holder.kwargs, dict(holder.kwargs))
 
 
 if __name__ == "__main__":
